82bc4a07c5e8326a80a92ad13fae52724b85c243
nguyenngocbich181106 <nguyenngocbich181106@gmail.com>
2024-11-28 22:21:59 +0700

    train2

---
+++ c++.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+/*Kieu du lieu
+int
+long long
+char
+float
+double
+bool: true, false
+
+Bien
+Kieu_du_lieu ten_bien;
+int dien_tich;
+int a, b, c;
+float ban_kinh;
+double chu_vi;
+Khong bat dau bang chu so
+khong duoc co dau cach
+khong duoc trung voi cac keyword*/
+int main(){
+	/*cout<<"Xin chao!\n";// hoac cout<<"xin chao!"<<endl;
+	cout<<"helo";
+	//int a, b, c;
+	int a = 100, b = 200, c = 300;
+	cout << a << " " << b << " " << c <<endl;*/
+	int a, b, c;
+	cin >>a;// nhap gia tri cho a tu ban phim
+	cout << a << endl;//in ra gia tri cua a ra man hinh
+
+	return 0;
+}
+ 
